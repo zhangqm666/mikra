daa3d66a27f31d49df5ff6dcfb9d4948abd990fa
bole <bole@C3PO>
2014-02-02 21:15:35 +0100

    xml output

--- intrastat/l10n_hr_intrastat.py
+++ intrastat/l10n_hr_intrastat.py
@@ -24,15 +24,19 @@ from tools.sql import drop_view_if_exists
 from decimal_precision import decimal_precision as dp
 import time
 from datetime import datetime, date
+from lxml import etree
+from psycopg2.errorcodes import NO_DATA_FOUND
+import base64
+#import XML_Generator
 
-
-
+NO_DATA = 'NO_DATA'
 
 class l10n_hr_intrastat(osv.Model):
     _name='l10n.hr.intrastat'
     _description = "Croatian intrastat reports"
    
-    
+    #def count_lines(self, cr, uid, field, value, context=None):
+        
      
     _columns = {
                 'state':fields.selection([('draft','Draft'),
@@ -61,6 +65,116 @@ class l10n_hr_intrastat(osv.Model):
                  'state':'draft',
                  'vrsta':'I',
                  }
+    
+    
+    def header_fill(self, cr, uid, envelope, context=None):
+        header = etree.SubElement(envelope,'Header')
+        if context is None:
+            context={}
+        company_id = context.get('company_id') or 1
+        company = self.pool.get('res.partner').browse(cr, uid, company_id)
+        
+        tok_robe = etree.SubElement(header,'FlowOfGoods')
+        tok_robe.text='1'
+        ###
+        izv_jed = etree.SubElement(header,'PSI')
+        jed_id = etree.SubElement(izv_jed,'PSIId')
+        jed_id_cc = etree.SubElement(jed_id,'PSICountryCode')
+        jed_id_cc.text = company.country_id.code
+        jed_id_vat = etree.SubElement(jed_id,'PSININumber')
+        jed_id_vat.text = company.vat[2:]
+        
+        jed_name = etree.SubElement(izv_jed,'PSIName')
+        jed_name.text = company.name
+        jed_adresa = etree.SubElement(izv_jed,'PSIAddress')
+        jed_adresa.text = company.street
+        if company.street2:
+            jed_adresa.text += ', '+ company.street2
+        jed_adresa.text += ', ' + company.city
+        
+        period = etree.SubElement(header,'ReferencePeriod')
+        period.text = '2014-01'
+        
+        #declarant = etree.SubElement(int_header,'Declarant')
+        #dec_id = etree.SubElement(declarant,'DeclarantId')
+        #dec_id_cc = etree.SubElement(dec_id,'DeclarantCountryCode')
+        #dec_id_vat = etree.SubElement(dec_id,'DeclarantNINumber')
+        #dec_name = etree.SubElement(declarant,'DeclarantName')
+        #dec_address = etree.SubElement(declarant,'DeclarantAddress')
+        #dec_country = etree.SubElement(declarant,'DeclarantCountry')
+        
+        dep_type = etree.SubElement(header,'IntrastatDepartment')
+        #TODO : generira se automatski?
+        
+        rep_type = etree.SubElement(header,'ReportType')
+        rep_type.text = 'I'
+        rep_date= etree.SubElement(header,'ReportDate')
+        rep_date.text = datetime.strftime(datetime.now(),"%Y-%m-%d")
+        return header
+    
+    def declaration_fill(self,cr, uid, envelope):
+        declaration = etree.SubElement(envelope, 'declaration')
+        datas = self.pool.get('l10n.hr.intrastat.line')
+        data_ids = datas.search(cr, uid, [('type','=','1')])
+        #ITERCIJA PO PROIZVODIMA
+        br = 0
+        for p in datas.browse(cr, uid, data_ids):
+            br +=1
+            item = etree.SubElement(declaration,'Item')
+            redni_broj = etree.SubElement(item,'ItemNr')
+            redni_broj.text = str(br)
+            sifra_robe = etree.SubElement(item,'CN8Code')
+            sifra_robe.text = p.sifra_robe and p.sifra_robe or NO_DATA
+            opis_robe = etree.SubElement(item,'GoodsDescription')
+            opis_robe.text = p.opis_robe and p.opis_robe or NO_DATA
+            sifra_zemlje = etree.SubElement(item,'DestinationCountryCode')
+            sifra_zemlje.text = p.zemlja and p.zemlja or NO_DATA
+            incoterm = etree.SubElement(item,'DeliveryTerms')
+            incoterm.text = p.incoterm_id and p.incoterm_id.code or NO_DATA
+            vrsta_posla = etree.SubElement(item, 'NatureOfTransaction')
+            vrsta_posla = p.vrsta_posla and p.vrsta_posla or NO_DATA
+            vrsta_prometa = etree.SubElement(item, 'TransportMode')
+            #vrsta_prometa.text = 
+            porijeklo_robe = etree.SubElement(item, 'CountryOfOriginCode')
+            porijeklo_robe.text = p.porijeklo_robe and p.porijeklo_robe or NO_DATA
+            neto_masa = etree.SubElement(item, 'NetWeight')
+            neto_masa.text = p.neto_masa and p.neto_masa or NO_DATA
+            kolicina_jm = etree.SubElement(item, 'QuantityInSU')
+            kolicina_jm = p.kolicina and p.kolicina or NO_DATA
+            fakturna = etree.SubElement(item, 'InvoicedAmount')
+            fakturna = p.fak_vrijedi and p.fak_vrijedi or NO_DATA
+            statisticka = etree.SubElement(item, 'StatisticalValue')
+            statisticka.text = p.stat_vrijedi and p.stat_vrijedi or NO_DATA 
+            
+        
+        return declaration, br
+    
+    def export_to_xml(self, cr, uid, ids, context=None):
+        report = etree.Element('IR001A') #, attrib=None, nsmap=None, **_extra)
+        envelope = etree.SubElement(report,'Element')    
+        #HEADER
+        header = self.header_fill(cr, uid, envelope)     
+        #DECLARATION
+        declaration , rb = self.declaration_fill(cr, uid, envelope) #etree.SubElement(envelope, 'declaration')
+        total_items = etree.SubElement(header,'TotalItems')
+        total_items.text = str(rb)
+        
+        xml_string = etree.tostring(report, pretty_print=True)  
+        attach_id = self.attach_xml_file(cr, uid, ids, xml_string, context)
+        return True
+    
+    def attach_xml_file(self, cr, uid, ids, xml_string, context=None):
+        assert len(ids) == 1, "Only one ID accepted"
+        #joppd_obj= self.pool.get('l10n.hr.joppd')
+        #jop_obj= joppd_obj.browse(cr, uid, ids[0])
+        file_name = '-'.join(['INTRASTAT' , 'I', datetime.strftime(datetime.now(),"%Y-%m-%d")])+'.xml'
+        attach_name = file_name
+        attach_obj = self.pool.get('ir.attachment')
+        context.update({'default_res_id': ids[0], 'default_res_model': 'l10n.hr.intrastat'})
+        attach_id = attach_obj.create(cr, uid, {'name': attach_name, 
+                                                'datas': base64.encodestring(xml_string), 
+                                                'datas_fname': file_name}, context=context)
+        return attach_id
     
     def fill_data_wiz(self, cr, uid, ids, context=None):
         #pretpostavljam da je single id
@@ -112,7 +226,7 @@ class l10n_hr_intrastat_line(osv.Model):
                 'type':fields.selection([('1','Primitak'),('2','otprema')],'Tok robe'),
                 'redni_br':fields.integer('Redni broj'),
                 'product_id':fields.many2one('product.product','Proizvod'),
-                'sifra_robe':fields.char("Intrastat code", size=10),
+                'sifra_robe':fields.char("Intrastat code", size=8),
                 'opis_robe':fields.char('Opis robe', size=256),
                 'porijeklo_robe':fields.char("Zemlja porijekla robe", size=2),
                 'neto_masa':fields.float("Netto masa", digits=(16,3)),
@@ -120,6 +234,7 @@ class l10n_hr_intrastat_line(osv.Model):
                 'zemlja':fields.char('Zemlja', size=2),
                 'incoterm_id':fields.many2one('stock.incoterms','Incoterm'),
                 'vrsta_posla':fields.many2one('intrastat.transaction.type','Vrsta posla'),
+                #vrsta prometa
                 'fak_vrijedi':fields.float('Fakturna vrijednost', digits=(16,2)),
                 'stat_vrijedi':fields.float('Statistička vrijednost', digits=(16,2))
                 }
